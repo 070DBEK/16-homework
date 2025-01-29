5475ae3c7741568f9030de215016e47373cc1221
Ozodbek <ahmadovozodbek80@gmail.com>
2025-01-29 21:17:21 +0500

    Added __pycache__ to .gitignore

--- brands/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-from .base_models import BaseModel
-
-
-class Brand(BaseModel):
-    logo = models.ImageField(upload_to='brands_images/')
--- catalogs/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-from brands.base_models import BaseModel
-
-
-class Category(BaseModel):
-    pass
